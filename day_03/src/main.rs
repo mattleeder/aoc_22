5be0067abaf099eccb275d9c0a1b338e70a893ca
Matt <mattleeder@posteo.co.uk>
2022-12-04 19:10:53 +0000

    Day 3 Part 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -4,25 +4,35 @@ use std::collections::HashMap;
 fn main() {
     let keys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let mut char_map: HashMap<char, usize> = HashMap::new();
-    let mut appeared: [usize; 58] = [0; 58];
+    let appeared: [usize; 58] = [0; 58];
 
     for (i, c) in keys.chars().enumerate() {
         char_map.insert(c, i + 1);
     }
 
-    let contents = utils::read_file().unwrap();
+    let binding = utils::read_file().unwrap();
+    let mut contents = binding.lines();
 
     let mut total_priorty = 0;
 
-    for line in contents.lines() {
+    while let (Some(line1), Some(line2), Some(line3)) =
+        (contents.next(), contents.next(), contents.next())
+    {
         let mut seen = appeared.clone();
-        let half = line.len() / 2;
-        for letter in line[..half].chars() {
+        println!("NEW CHUNK!");
+        println!("{}\n{}\n{}", line1, line2, line3);
+        for letter in line1.chars() {
             seen[letter as usize - 65] = 1;
         }
-        for letter in line[half..].chars() {
+        for letter in line2.chars() {
             if seen[letter as usize - 65] == 1 {
+                seen[letter as usize - 65] = 2;
+            }
+        }
+        for letter in line3.chars() {
+            if seen[letter as usize - 65] == 2 {
                 total_priorty += char_map[&letter];
+                println!("{}\n", letter);
                 break;
             }
         }
