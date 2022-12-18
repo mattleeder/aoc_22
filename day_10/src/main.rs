9f8f0e6840cb05a70e00b58c51000c3eac7b6372
Matt <mattleeder@posteo.co.uk>
2022-12-18 20:22:57 +0000

    Day 10 Part 2

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,43 +1,22 @@
 use common::utils;
-
-struct CycleListener {
-    start: u32,
-    step: u32,
-    total: i32,
-}
-
-impl CycleListener {
-
-    fn new(start: u32, step: u32) -> CycleListener {
-        CycleListener{ start, step, total: 0 }
-    }
-
-    fn is_speak_cycle(&self, cycles: u32) -> bool {
-        if cycles >= self.start {
-            return ((cycles - self.start) % self.step) == 0
-        }
-        false
-    }
-
-}
+use std::fs::File;
+use std::io::Write;
 
 struct CPU {
     cycles: u32,
     x_register: i32,
-    listener: CycleListener,
+    log: File,
 }
 
 impl CPU {
 
-    fn new(listener: CycleListener) -> CPU {
-        CPU { cycles: 0, x_register: 1, listener}
+    fn new(log: File) -> CPU {
+        CPU { cycles: 0, x_register: 1, log }
     }
 
     fn increment_cycles(&mut self) {
+        self.draw();
         self.cycles += 1;
-        if self.listener.is_speak_cycle(self.cycles) {
-            self.speak();
-        }
     }
 
     fn noop(&mut self) {
@@ -50,10 +29,21 @@ impl CPU {
         self.x_register += x;
     }
 
-    fn speak(&mut self) {
-        self.listener.total += self.x_register * self.cycles as i32;
-        println!("{}", self.cycles - self.listener.start);
-        println!("Cycles: {}, Added: {}", self.cycles, self.x_register * self.cycles as i32);
+    fn draw(&mut self) {
+        let log_line = format!("During Cycle: {}\t\tX Register: {}\n", self.cycles + 1, self.x_register);
+        self.log.write_all(log_line.as_bytes()).unwrap();
+        if (self.cycles) % 40 == 0 {
+            print!("    Cycle: {}\n", self.cycles);
+        }
+        if self.is_sprite_aligned() {
+            print!("#");
+        } else {
+            print!(".");
+        }
+    }
+
+    fn is_sprite_aligned(&self) -> bool {
+        self.x_register.abs_diff((self.cycles % 40) as i32) <= 1
     }
 
     fn read_instruction(&mut self, line: &str) {
@@ -68,13 +58,12 @@ impl CPU {
 }
 
 fn main() {
+    let mut file = File::create("aoc_day_10_log.txt").unwrap();
+    file.write_all(b"NEW LOG\n=====================================================\n").unwrap();
     let contents = utils::read_file().unwrap();
-    let listener = CycleListener::new(20, 40);
-    let mut cpu = CPU::new(listener);
+    let mut cpu = CPU::new(file);
 
     for line in contents.lines() {
         cpu.read_instruction(line);
     }
-
-    println!("Part 1: {}", cpu.listener.total);
 }
